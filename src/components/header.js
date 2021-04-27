6b3e744aee4a5588a366083618cf530ee9973a36
Nicolas Bertini Argañaras <nicolas_bertini@hotmail.com>
2021-04-27 12:14:42 -0300

    Configuracion inicial de plugins en gatsby.config.js, y consulta a graphQL de una imagen para el background-image del inicio

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,38 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import React from "react"
+import { css } from "@emotion/react"
+import Navegacion from "./Navegacion"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+const Header = props => {
+  return (
+    <header
+      css={css`
+        background-color: #222;
+        padding: 1rem;
+      `}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+      <div
+        css={css`
+          max-width: 1200px;
+          margin: 0 auto;
+          @media (min-width: 768px) {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+        `}
+      >
+        <h1
+          css={css`
+            color: #fff;
+            text-align: center;
+          `}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+          Hotel Gatsby
+        </h1>
+        <Navegacion></Navegacion>
+      </div>
+    </header>
+  )
 }
 
 export default Header
